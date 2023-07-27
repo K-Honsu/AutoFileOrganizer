38a76289bb4afe0828835dd1f91312d4fd5623d4
K-Honsu <feranmia51@gmail.com>
2023-07-27 22:57:13 +0100

    Initial commit

---
+++ automate-directory.py
@@ -0,0 +1,69 @@
+import os
+from os.path import splitext, exists, join
+import sys
+import time
+import logging
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+source_dir = '/Users/oluwaferanmiadeyemi/Downloads'
+destination_sound = '/Users/oluwaferanmiadeyemi/Downloads/downloaded sounds'
+destination_music = '/Users/oluwaferanmiadeyemi/Downloads/downloaded music'
+destination_video = '/Users/oluwaferanmiadeyemi/Downloads/downloaded videos'
+destination_image = '/Users/oluwaferanmiadeyemi/Downloads/downloaded images'
+
+
+def makeUnique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    # * IF FILE EXISTS, ADDS NUMBER TO THE END OF THE FILENAME
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+
+    return name
+
+def move(destination, entry, name):
+    file_exists = os.path.exists(destination + "/" + name)
+    if file_exists:
+        unique_name = makeUnique(name)
+        os.rename(entry, unique_name)
+    shutil.move(entry, destination)
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with os.scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                destination = source_dir
+                if name.endswith('.wav') or name.endswith('.mp3'):
+                    if entry.stat().st_size < 25000000 or "SFX" in name:
+                        destination = destination_sound
+                    else:
+                        destination = destination_music
+                    move(destination, entry,name)
+                elif name.endswith('.mov') or name.endswith('.mp4'):
+                    destination = destination_video
+                    move(destination, entry, name)
+                elif name.endswith('.jpg') or name.endswith('.png'):
+                    destination = destination_image
+                    move(destination, entry, name)
+            
+        
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    # path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
